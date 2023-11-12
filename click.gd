ccde8d11c3d440466096cbf4bfebfd605eb2ed7f
Swee <sweeistaken@gmail.com>
2023-11-11 19:42:55 -0800

    e

---
+++ click.gd
@@ -0,0 +1,19 @@
+extends AnimatedSprite2D
+@onready var _animated_sprite = $click
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	pass
+	
+func _input(event):
+	if event.is_action_pressed("clicked!"):
+		_animated_sprite.play("click")
+		pass
+	pass
+	
